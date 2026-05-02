3b48dff901befc139adac17fc74ee1a34082ba79
eyacherif03 <eya.cherif@etudiant-fst.utm.tn>
2026-05-02 19:51:01 +0100

    add k8s and terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "water-potability-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.0.0"
+
+  cluster_name    = "water-potability-cluster"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    nodes = {
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
+      instance_types = ["t3.small"]
+    }
+  }
+}
