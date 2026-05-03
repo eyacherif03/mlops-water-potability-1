ca92a5cf06d57fc9f2708f616c404d704bc60037
eyacherif03 <eya.cherif@etudiant-fst.utm.tn>
2026-05-03 14:40:15 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
-pterraform {
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.83.0"
     }
   }
 }
 
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
 data "aws_availability_zones" "available" {}
@@ -17,24 +17,19 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
 
-  name = "water-potability-vpc"
-  cidr = "10.0.0.0/16"
-
+  name            = "water-potability-vpc"
+  cidr            = "10.0.0.0/16"
   azs             = data.aws_availability_zones.available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
-
-  tags = {
-    Project = "water-potability"
-  }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
+  version = "20.8.0"
 
   cluster_name    = "water-potability-cluster"
   cluster_version = "1.29"
@@ -42,7 +37,8 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = false  # ← CRITIQUE pour AWS Academy
 
   eks_managed_node_groups = {
     nodes = {
@@ -51,9 +47,5 @@ module "eks" {
       min_size       = 1
       instance_types = ["t3.small"]
     }
-  }
-
-  tags = {
-    Project = "water-potability"
   }
 }
