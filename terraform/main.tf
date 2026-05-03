f57c81bba1df3abb37ca9e829949b961be6cc483
eyacherif03 <eya.cherif@etudiant-fst.utm.tn>
2026-05-03 15:27:59 +0100

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,21 +29,19 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.0"
+  version = "19.21.0"
 
   cluster_name    = "water-potability-cluster"
   cluster_version = "1.29"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = false
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     nodes = {
-      desired_size   = 1
-      max_size       = 2
+      desired_size   = 2
+      max_size       = 3
       min_size       = 1
       instance_types = ["t3.small"]
     }
