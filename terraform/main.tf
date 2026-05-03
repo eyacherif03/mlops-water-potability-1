2f4618c93613a96f4990e812db4b9cea420c3ad5
Eya Cherif <eya.cherif@etudiant-fst.utm.tn>
2026-05-03 14:28:47 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,45 +2,43 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.83.0" 
+      version = "~> 5.83.0"
     }
   }
 }
 
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
 data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+  version = "5.0.0"
 
-  name = "water-potability-vpc"
-  cidr = "10.0.0.0/16"
-
+  name            = "water-potability-vpc"
+  cidr            = "10.0.0.0/16"
   azs             = data.aws_availability_zones.available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
-
-  tags = {
-    Project = "water-potability"
-  }
 }
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.0" 
+  version = "20.8.0"
 
   cluster_name    = "water-potability-cluster"
-  cluster_version = "1.28"
+  cluster_version = "1.29"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = false  
 
   eks_managed_node_groups = {
     nodes = {
@@ -48,14 +46,6 @@ module "eks" {
       max_size       = 2
       min_size       = 1
       instance_types = ["t3.small"]
-      capacity_type  = "ON_DEMAND"
     }
-  }
-
-  # Requis en v20 pour accorder l'accès au cluster
-  enable_cluster_creator_admin_permissions = false
-
-  tags = {
-    Project = "water-potability"
   }
 }
