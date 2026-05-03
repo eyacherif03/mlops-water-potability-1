1d5d8d50f9e934ae983c894bc0e2c449a634f36c
Eya Cherif <eya.cherif@etudiant-fst.utm.tn>
2026-05-03 13:21:37 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,17 +31,15 @@ module "vpc" {
     Project = "water-potability"
   }
 }
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.31.2"
+  version = "~> 20.0"
 
   cluster_name    = "water-potability-cluster"
   cluster_version = "1.28"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
@@ -50,9 +48,12 @@ module "eks" {
       max_size       = 2
       min_size       = 1
       instance_types = ["t3.small"]
-      capacity_type   = "ON_DEMAND"
+      capacity_type  = "ON_DEMAND"
     }
   }
+
+  # Requis en v20 pour accorder l'accès au cluster
+  enable_cluster_creator_admin_permissions = true
 
   tags = {
     Project = "water-potability"
