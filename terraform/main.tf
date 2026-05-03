79ac43b4bad218f654fbbced01defad0f4ef20bf
Eya Cherif <eya.cherif@etudiant-fst.utm.tn>
2026-05-03 12:13:10 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.4"
+  version = "19.21.0"
 
   cluster_name    = "water-potability-cluster"
   cluster_version = "1.29"
